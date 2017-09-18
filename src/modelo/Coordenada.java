80945d273c207764334319bd669e560ea09b8a88
alu <alu@grado1>
2017-09-18 21:55:47 +0200

    hola

---
+++ src/modelo/Coordenada.java
@@ -0,0 +1,34 @@
+package modelo;
+
+public class Coordenada {
+	private int x;
+	private int y;
+	private static int NUMERO_COORDENADA;
+	
+	public Coordenada(int x, int y) {
+		this.x=x;
+		this.y=y;
+		NUMERO_COORDENADA++;
+	}
+	
+	public Coordenada(Coordenada otra) {
+		x=otra.x;
+		y=otra.y;
+	}
+	
+	public int getNumeroCoordenada() {
+		return NUMERO_COORDENADA;
+	}
+	
+	public String toString() {
+		return "Coordenada [x=" + x + ", y=" + y + "]";
+	}
+
+	public int getX() {return x;}
+
+	public int getY() {return y;}
+	
+	
+	
+	
+}
