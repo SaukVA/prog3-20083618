ee8cd3971b444ca6a342b8fa5b1a76bb68218686
alu <alu@grado1>
2017-09-20 12:28:25 +0200

    Cambio

--- src/modelo/Coordenada.java
+++ src/modelo/Coordenada.java
@@ -1,5 +1,19 @@
 package modelo;
 
+/**
+ * En esta practica se pretende que se comprenda el leguage Java
+ * con la traduccion de un esquema de UML a lenguaje Java.
+ * 
+ * Los metodos de los que consta este objeto son: constructor,
+ * geters de las variables(x,y,NUMERO_COORDENADA), un metodo 
+ * que se encarga de traducir a String  las variables de 
+ * las coordenada(toString()), otro metodo encargado de 
+ * la suma de dos  Coordenadas (suma()) y por ultimo el metodo 
+ * equals() encatgado de comprobar si las dos coordenadas son iguales.
+ * 
+ * @author Saul Verdu Aparicio DNI:20083618-H
+ **/
+
 public class Coordenada {
 	private int x;
 	private int y;
