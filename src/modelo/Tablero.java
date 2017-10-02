ace54d06beac512feecc05133d997ba73a48c212
root <root@clL18-20>
2017-10-02 12:57:22 +0200

    cambio

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
+
 public class Tablero {
 	private static final int Max=1;
 	private List<Coordenada> dimensiones = new ArrayList<Coordenada>();
@@ -56,11 +57,50 @@ public class Tablero {
 		}
 	}
 	
-	void ArrayList<Coordenada> getPosicionesVecinasCCW(Coordenada posicion){
+	public ArrayList<Coordenada> getPosicionesVecinasCCW(Coordenada posicion) {
 		
-		ArrayList<Coordenada> c = new ArrayList<Coordenada> ();
-		int x = posicion.getX();
+		ArrayList<Coordenada> resp =new ArrayList<Coordenada>();
 		
-		for()
+		for(int i=0; i<=7;i++) {
+			switch(i) {
+				case 0: 
+					resp.add(new Coordenada(posicion.getX()-1,posicion.getY()-1));
+					break;
+				case 1: 
+					resp.add(new Coordenada(posicion.getX()-1,posicion.getY()));
+					break;
+				case 2: 
+					resp.add(new Coordenada(posicion.getX()-1,posicion.getY()+1));
+					break;
+				case 3: 
+					resp.add(new Coordenada(posicion.getX(),posicion.getY()+1));
+					break;
+				case 4: 
+					resp.add(new Coordenada(posicion.getX()+1,posicion.getY()+1));
+					break;
+				case 5: 
+					resp.add(new Coordenada(posicion.getX()+1,posicion.getY()));
+					break;
+				case 6: 
+					resp.add(new Coordenada(posicion.getX()+1,posicion.getY()-1));
+					break;
+				case 7: 
+					resp.add(new Coordenada(posicion.getX(),posicion.getY()-1));
+					break;
+			}
+		}
+		return resp;
+	}
+	
+	//boolean cargaPatron(Patron patron, Coordenada coordenadaInicial) {}
+	
+	boolean contiene(Coordenada posicion) {
+		return celdas.containsKey(posicion);
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		return null;
 	}
 }
