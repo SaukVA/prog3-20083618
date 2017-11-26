0a5f50da7bae71b3e3da237836fb7a84585c750d
kowsv <kowsv@DESKTOP-31F7PN5>
2017-11-26 18:20:13 +0100

    Mod

--- src/entradasalida/textoplano/ParserTablero2D.java
+++ src/entradasalida/textoplano/ParserTablero2D.java
@@ -2,20 +2,25 @@ package entradasalida.textoplano;
 
 import modelo.Coordenada2D;
 import modelo.Tablero;
-import modelo.Tablero2D;
+import modelo.TableroCeldasCuadradas;
 import modelo.excepciones.ExcepcionArgumentosIncorrectos;
 import modelo.excepciones.ExcepcionCoordenadaIncorrecta;
 import modelo.excepciones.ExcepcionEjecucion;
+import modelo.excepciones.ExcepcionPosicionFueraTablero;
+
 import static modelo.EstadoCelda.*;
+
+import entradasalida.excepciones.ExcepcionLectura;
 
 public class ParserTablero2D {
 
 	public ParserTablero2D() {}
 	
-	public Tablero leeTablero(String cadena) {
+	public Tablero leeTablero(String cadena) throws ExcepcionLectura, ExcepcionPosicionFueraTablero {
 		
 		boolean error=false;
 		char c;
+		int alto=0, ancho=0;
 		
 		try {
 			if (cadena==null) {
@@ -29,12 +34,43 @@ public class ParserTablero2D {
 				if(c!='*' && c!=' ' && c!='\n') {
 					error=true;
 				}
+				if(c=='\n') {alto++;}
+				if(alto==0) {ancho++;}
 			}
 			
+			if(cadena.charAt(tam-1)!='\n'&& !error) {alto++;}
+			
+			TableroCeldasCuadradas t = new TableroCeldasCuadradas(ancho,alto);
+			
+			if(error || tam==0) {
+				throw new ExcepcionLectura();
+			}
+			else {
+				for(int j=0;j<ancho;j++) {
+					c=cadena.charAt(j);
+					int k=0,p=0;
+					
+					if(c!='\n') {
+						switch(c) {
+							case('*'):
+								t.setCelda(new Coordenada2D(k,p), VIVA);
+								break;
+							case(' '):
+								t.setCelda(new Coordenada2D(k,p), MUERTA);
+								break;
+						}
+						k++;
+					}
+					else {
+						k=0;
+						p++;
+					}
+				}
+			}
+			return t;
 		}
 		catch(ExcepcionCoordenadaIncorrecta e) {
 			throw new ExcepcionEjecucion(e);
 		}
-		return null;
 	}
 }
--- src/mains/MainPruebas.java
+++ src/mains/MainPruebas.java
@@ -5,43 +5,19 @@ import modelo.TableroCeldasCuadradas;
 import modelo.EstadoCelda;
 import modelo.Juego;
 import modelo.ReglaConway;
+import modelo.Tablero1D;
 import modelo.excepciones.ExcepcionCoordenadaIncorrecta;
 import modelo.excepciones.ExcepcionPosicionFueraTablero;
+import entradasalida.excepciones.ExcepcionLectura;
+import entradasalida.textoplano.*;
 
 public class MainPruebas {
 
-	public static void main(String[] args) throws ExcepcionCoordenadaIncorrecta, ExcepcionPosicionFueraTablero {
+	public static void main(String[] args) throws ExcepcionCoordenadaIncorrecta, ExcepcionPosicionFueraTablero, ExcepcionLectura {
 
-			
-			TableroCeldasCuadradas t1 = new TableroCeldasCuadradas(5,5);
-			ReglaConway r =new ReglaConway();
-			Coordenada2D c = new Coordenada2D(0,1);
-			Coordenada2D c2 = new Coordenada2D(0,2);
-			Coordenada2D c3 = new Coordenada2D(0,3);
-			Coordenada2D c4 = new Coordenada2D(0,0);
-			
-			System.out.println("Hola");
-			System.out.println(t1.toString());
-			
-			t1.setCelda(c,EstadoCelda.VIVA);
-			t1.setCelda(c2,EstadoCelda.VIVA);
-			t1.setCelda(c3,EstadoCelda.VIVA);
-			t1.setCelda(c4,EstadoCelda.VIVA);
-			
-			Juego juego = new Juego(t1, r);
-			
-			System.out.println(t1.toString());
-			
-			juego.actualiza();
-			System.out.println(t1.toString());
-			
-			t1.setCelda(c,EstadoCelda.MUERTA);
-			t1.setCelda(c2,EstadoCelda.MUERTA);
-			System.out.println(t1.toString());
-			
-			juego.actualiza();
-			System.out.println(t1.toString());
-			
-			
+			ParserTablero1D p= new ParserTablero1D ();
+			String s= new String("** **");
+			Tablero1D t= (Tablero1D) p.leeTablero(s);
+			System.out.println(t.toString());
 	}
 }
